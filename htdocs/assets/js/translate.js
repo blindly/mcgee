463410466c78ed34cb2d7e32dce338df71c79a7c
blindly <@blindly>
2015-12-04 06:04:45 -0500

    initial import

---
+++ htdocs/assets/js/translate.js
@@ -0,0 +1,4 @@
+// 8-6-2015
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true, gaId: 'UA-66051813-1'}, 'google_translate_element');
+}
